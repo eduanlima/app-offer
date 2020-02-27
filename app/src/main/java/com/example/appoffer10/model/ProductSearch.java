3d3d103a65b5013781d9de77548385a72138ddda
eduanlima <eduanlima@outlook.com>
2020-02-27 13:01:50 -0300

    Adapter offer search

---
+++ app/src/main/java/com/example/appoffer10/model/ProductSearch.java
@@ -0,0 +1,72 @@
+package com.example.appoffer10.model;
+
+import java.io.Serializable;
+
+public class ProductSearch implements Serializable {
+
+    private Integer id;
+    private Double price;
+    private Integer store;
+
+    public ProductSearch(Integer id, Double price, Integer store) {
+        this.id = id;
+        this.price = price;
+        this.store = store;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getStore() {
+        return store;
+    }
+
+    public void setStore(Integer store) {
+        this.store = store;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        ProductSearch other = (ProductSearch) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "ProductSearch [id=" + id + ", price=" + price + ", store=" + store + "]";
+    }
+
+}
+
